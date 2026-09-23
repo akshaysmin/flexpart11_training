101390580a7a8f6858a0f7efb2b53b15707de7e2
Marie Mulder <marie.mulder@geosphere.at>
2026-09-23 19:05:24 +0000

    Adding volcano files

---
+++ volcanos/MAKE_RELEASES/release_parameters.py
@@ -0,0 +1,16 @@
+ventElevation       = 1725     # summit elevation (m asl)
+vlon                = -17.3333 # summit lon 
+vlat                = 64.4167  # summit lat
+particlesPerRelease = 1000     # number of particles per release
+numRelLevels        = 5       # number of release levels
+relHeightInt        = 1000      # releases height interval
+speciesIndices      = [201, 202, 203, 204]
+#speciesIndices      = [101, 102, 103, 104, 105, 106, 107, 108, 109]
+startTime           = 2011052118 # Start time of the releases
+stopTime            = 2011052122 # Start time of the releases
+elevCode            = 1      # Reference level 1=above ground, 2=above sea level, 3 for pressure in hPa
+releaseFileDir      = '/export/home/umwelt/mmulder/2026_FP_course/2026_volc/rundir/options/'
+
+
+
+
